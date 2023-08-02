09b530b043f670f1308ec88b3adc6d5f6e848f4d
Deva0813 <devanand151101@gmail.com>
2023-08-02 07:37:00 +0530

    mongo db integration

--- app/account/page.tsx
+++ app/account/page.tsx
@@ -2,7 +2,8 @@
 import clsx from "clsx";
 import NavBarComp from "../components/NavBarComp/page";
 import { Ubuntu } from "next/font/google";
-import { useState } from "react";
+import { useState ,useEffect} from "react";
+
 
 const titlefont = Ubuntu(
     {
@@ -16,6 +17,52 @@ export default function Account() {
     const [accInfoVisible, setAccInfoVisible] = useState(false);
     const [changePassVisible, setChangePassVisible] = useState(true);
     const [deleteAccVisible, setDeleteAccVisible] = useState(true);
+
+    const [accInfo, setAccInfo] = useState({
+        firstName: "",
+        lastName: "",
+        age: "",
+        gendere:"",
+        ph_no:"",
+        email: "",
+        password: "",
+        username: "",
+    })
+
+    const handleChange = (e:any) => {
+        const { name, value } = e.target;
+        setAccInfo(prevState => ({
+            ...prevState,
+            [name]: value
+        }))
+    }
+
+    useEffect(() => {
+        console.log(accInfo);
+
+        var myHeaders = new Headers();
+        myHeaders.append("Content-Type", "application/json");
+        myHeaders.append("Access-Control-Request-Headers", "*");
+        myHeaders.append("api-key", process.env.API_KEY as string);
+        myHeaders.append("Accept", "application/json");
+
+        var raw = JSON.stringify({
+        "dataSource": process.env.DATASOURCE as string,
+        "database": process.env.DATABASE as string,
+        "collection": "users",
+        });
+
+        fetch("/v1/action/find", {
+        method: 'POST',
+        headers: myHeaders,
+        body: raw,
+        redirect: 'follow'
+        })
+        .then(response => response.json())
+        .then(result => console.log(result))
+
+
+    }, [accInfo]);
 
     return (
         <div className="account_page">
@@ -80,19 +127,19 @@ export default function Account() {
                                     <div className="personal-info grid grid-flow-row gap-2 md:gap-0 md:grid-cols-2 w-full ">
                                         <div className="flex flex-col md:px-5 md:pt-5 gap-2 md:w-full ">
                                             <label className="text-sm font-semibold pl-2 " >First Name</label>
-                                            <input className="border-2 text-cursor border-btn_add-700 rounded-md p-2 focus:border-btn_add-800 focus:outline-none transition-all ease-in-out delay-150" type="text" placeholder="First Name" />
+                                            <input className="border-2 text-cursor border-btn_add-700 rounded-md p-2 focus:border-btn_add-800 focus:outline-none transition-all ease-in-out delay-150" type="text" name="firstName" onChange={handleChange} placeholder="First Name" />
                                         </div>
                                         <div className="flex flex-col md:px-5 md:pt-5 gap-2 md:w-full ">
                                             <label className="text-sm font-semibold pl-2 " >Last Name</label>
-                                            <input className="border-2 text-cursor border-btn_add-700 rounded-md p-2 focus:border-btn_add-800 focus:outline-none transition-all ease-in-out delay-150" type="text" placeholder="Last Name" />
+                                            <input className="border-2 text-cursor border-btn_add-700 rounded-md p-2 focus:border-btn_add-800 focus:outline-none transition-all ease-in-out delay-150" type="text" name="lastName" onChange={handleChange} placeholder="Last Name" />
                                         </div>
                                         <div className="flex flex-col md:px-5 md:pt-5 gap-2 md:w-full ">
                                             <label className="text-sm font-semibold pl-2 " >Age</label>
-                                            <input className="border-2 text-cursor border-btn_add-700 rounded-md p-2 focus:border-btn_add-800 focus:outline-none transition-all ease-in-out delay-150" type="text" placeholder="Age" />
+                                            <input className="border-2 text-cursor border-btn_add-700 rounded-md p-2 focus:border-btn_add-800 focus:outline-none transition-all ease-in-out delay-150" type="text" name="age" onChange={handleChange} placeholder="Age" />
                                         </div>
                                         <div className="flex flex-col md:px-5 md:pt-5 gap-2 md:w-full ">
                                             <label className="text-sm font-semibold pl-2 ">Gender</label>
-                                            <select className="border-2 default border-btn_add-700 rounded-md p-2 focus:border-btn_add-800 focus:outline-none transition-all ease-in-out delay-150" >
+                                            <select className="border-2 default border-btn_add-700 rounded-md p-2 focus:border-btn_add-800 focus:outline-none transition-all ease-in-out delay-150" name="gender"  onChange={handleChange}>
                                                 <option value="Select Gender ">Select Gender</option>
                                                 <option value="Male">Male</option>
                                                 <option value="Female">Female</option>
@@ -102,12 +149,12 @@ export default function Account() {
                                         </div>
                                         <div className="flex flex-col md:px-5 md:pt-5 gap-2 md:w-full ">
                                             <label className="text-sm font-semibold pl-2 " >Phone Number</label>
-                                            <input className="border-2 text-cursor border-btn_add-700 rounded-md p-2 focus:border-btn_add-800 focus:outline-none transition-all ease-in-out delay-150" type="tel" placeholder="Phone Number" />
+                                            <input className="border-2 text-cursor border-btn_add-700 rounded-md p-2 focus:border-btn_add-800 focus:outline-none transition-all ease-in-out delay-150" type="tel" name="ph_no" onChange={handleChange} placeholder="Phone Number" />
                                         </div>
                                     </div>
                                 </div>
 
-                                <div className="left-details flex flex-col xl:flex-row  border-b-2 border-btn_add-500 ">
+                                <div className="left-details flex flex-col xl:flex-row pb-5 xl:pb-0 border-b-2 border-btn_add-500 ">
                                     <div className="w-full xl:w-1/3 py-5 ">
                                         <p className="text-sm px-5 xl:p-5" >
                                             This name is displayed publicly and is used to identify you.
@@ -116,7 +163,7 @@ export default function Account() {
                                     <div className="personal-info grid grid-flow-row gap-2 md:gap-0 md:grid-cols-2 w-full ">
                                         <div className="flex flex-col md:px-5 md:pt-5 gap-2 md:w-full ">
                                             <label className="text-sm font-semibold pl-2 " >Username</label>
-                                            <input className="border-2 text-cursor border-btn_add-700 rounded-md p-2 focus:border-btn_add-800 focus:outline-none transition-all ease-in-out delay-150" type="text" placeholder="Username" />
+                                            <input className="border-2 text-cursor border-btn_add-700 rounded-md p-2 focus:border-btn_add-800 focus:outline-none transition-all ease-in-out delay-150" type="text" name="username" onChange={handleChange} placeholder="Username" />
                                         </div>
                                     </div>
                                 </div>
@@ -134,7 +181,7 @@ export default function Account() {
                                     <div className="personal-info grid grid-flow-row gap-2 md:gap-0 md:grid-cols-2 w-full ">
                                         <div className="flex flex-col md:px-5 md:pt-5 gap-2 md:w-full ">
                                             <label className="text-sm font-semibold pl-2 " >Email</label>
-                                            <input className="border-2 text-cursor border-btn_add-700 rounded-md p-2 focus:border-btn_add-800 focus:outline-none transition-all ease-in-out delay-150" type="email" placeholder="Email" />
+                                            <input className="border-2 text-cursor border-btn_add-700 rounded-md p-2 focus:border-btn_add-800 focus:outline-none transition-all ease-in-out delay-150" type="email" name="email" placeholder="Email"  onChange={handleChange}/>
                                         </div>
                                     </div>
                                 </div>
@@ -168,7 +215,7 @@ export default function Account() {
                                     </div>
                                     <div className="flex flex-col md:px-5 md:pt-5 gap-2 md:w-full ">
                                         <label className="text-sm font-semibold pl-2 " >New Password</label>
-                                        <input className="border-2 text-cursor border-btn_add-700 rounded-md p-2 focus:border-btn_add-800 focus:outline-none transition-all ease-in-out delay-150" type="password" placeholder="New Password" />
+                                        <input className="border-2 text-cursor border-btn_add-700 rounded-md p-2 focus:border-btn_add-800 focus:outline-none transition-all ease-in-out delay-150" type="password" name="password" onChange={handleChange} min={8} placeholder="New Password" />
                                     </div>
                                     <div className="flex flex-col md:px-5 md:pt-5 gap-2 md:w-fit">
                                         <button className=" rounded-md p-2 px-5 text-white hover:bg-green-600 bg-green-500 focus:outline-none pointer " >Change Password</button>
